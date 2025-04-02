bb5191d12c7d8f9f7496b35340bed24b15640799
Savan120 <patelsavan931406891@gmail.com>
2025-04-02 19:17:16 +0530

    Add the Webhhok event method on order, customer and product creation

--- shopify_connector/shopify_connector/customisation/api/webhook.py
+++ shopify_connector/shopify_connector/customisation/api/webhook.py
@@ -1,12 +1,12 @@
 import frappe
 import requests
-import datetime
 from frappe import _
 
 @frappe.whitelist(allow_guest=True)
 def receive_shopify_order():
     order_data = frappe.local.request.get_json()
     settings = frappe.get_doc("Shopify Connector Setting")
+    password = settings.access_token
     
     company_abbr = frappe.db.get_value("Company", settings.company, "abbr")
     sys_lang = frappe.get_single("System Settings").language or "en"
@@ -15,10 +15,16 @@ def receive_shopify_order():
     order_number = order_data.get("order_number")
     customer = order_data.get("customer", {})
     customer_name = customer.get("first_name", "") + " " + customer.get("last_name", "")
+    contact_email = customer.get("email")
     created_date = order_data.get("created_at", "").split("T")[0]
     items = order_data.get("line_items", [])
     shipping_lines = sum(float(line.get('price', 0)) for line in order_data.get('shipping_lines', []))
     total_tax = sum(float(tax.get("price", 0)) for tax in order_data.get("tax_lines", []))
+    billing = order_data.get('billing_address')
+    raw_billing_data = billing
+
+    shipping = order_data.get('shipping_address',False)
+    raw_shipping_data = shipping
     
     discount_info = order_data.get("discount_applications", [])
     discount_percentage = sum(float(dis.get("value", 0)) for dis in discount_info if dis.get("value_type") == "percentage")
@@ -30,6 +36,10 @@ def receive_shopify_order():
     if not customer_name:
         frappe.throw(_(f"Customer name is missing in the order id {order_number}."))
         
+    get_customer = frappe.db.get_value("Customer", {"shopify_email": customer.get("email")})
+    if not get_customer:
+        link_customer_and_address(raw_shipping_data, customer_name, contact_email)
+       
     
     sales_order = frappe.new_doc("Sales Order")
     sales_order.customer = customer_name.strip() or "Guest"
@@ -39,12 +49,13 @@ def receive_shopify_order():
     sales_order.transaction_date = created_date
     sales_order.delivery_date = frappe.utils.add_days(created_date, settings.delivery_after_days or 7)
     
+    images_src = []
     for item in items:
         product_id = item.get("product_id")
         item_name = item.get("name")
-        
-        if not frappe.db.exists("Item", {"shopify_id": product_id}):
-            frappe.throw("called")
+            
+            
+        if not frappe.db.get_value("Item", {"shopify_id": product_id}):
             new_item = frappe.new_doc("Item")
             new_item.item_code = f"Shopify-{product_id}"
             new_item.item_name = item_name
@@ -52,8 +63,24 @@ def receive_shopify_order():
             new_item.item_group = _( "Shopify Products", sys_lang)
             new_item.shopify_id = product_id
             new_item.shopify_selling_rate = item.get("price", 0)
-            new_item.flags.ignore_mandatory = True
-            print(f"\n\n\n\n new item: {new_item.__dict__}")
+            
+            response = requests.get(f'https://{shopify_url}/admin/api/2021-10/products/{product_id}.json', headers={'X-Shopify-Access-Token': password})
+            if response.status_code == 200:
+                product_data = response.json()['product']
+                images_src += [image['src'] for image in product_data['images']]
+            else:
+                print(f"Failed to fetch product details: {response.status_code} - {response.text}")
+            img_link  = images_src[0] if len(images_src)>0 else ''
+            
+            file_doc = frappe.new_doc("File")
+            file_doc.file_url = img_link
+            file_doc.is_private = 0
+            file_doc.flags.ignore_permissions = True
+            file_doc.insert(ignore_permissions=True)
+            file_doc.save()
+            new_item.image = file_doc.file_url if file_doc else '' 
+                
+            new_item.flags.ignore_permissions = 1
             new_item.insert(ignore_permissions=True)
             new_item.save()
         
@@ -68,12 +95,29 @@ def receive_shopify_order():
             "warehouse": settings.warehouse or f"Stores - {company_abbr}"
         })
     
-    sales_order.flags.ignore_mandatory = True
+    sales_order.flags.ignore_permissions = True
     sales_order.insert(ignore_permissions=True)
     sales_order.submit()
     
     frappe.msgprint(_("Sales Order created for order number: {0}").format(order_number))
-    return "success"
+    return "Success"
+
+def link_customer_and_address( raw_shipping_data, customer_name, contact_email):
+    if raw_shipping_data:
+        customer_shopify_email = contact_email
+        customer_exists = frappe.get_value("Customer", {"shopify_email": customer_shopify_email})
+        if not customer_exists:
+            customer = frappe.new_doc("Customer")
+        else:
+            customer = frappe.get_doc("Customer", {"shopify_email": customer_shopify_email})
+            old_name = customer.customer_name
+
+        customer.customer_name = customer_name
+        customer.shopify_email = customer_shopify_email
+        customer.shopify_email = customer_shopify_email
+        customer.flags.ignore_permission = True
+        customer.insert(ignore_permissions=True)
+        customer.save()
 
 
 @frappe.whitelist(allow_guest=True)
@@ -105,7 +149,7 @@ def customer_creation():
                 "link_doctype": "Customer",
                 "link_name": cus.name,
             })
-            cus_address.flags.ignore_mandatory = True
+            cus_address.flags.ignore_permissions = 1
             cus_address.insert(ignore_permissions=True)
             cus_address.save()
         
@@ -113,3 +157,54 @@ def customer_creation():
         frappe.msgprint(_("Customer created for email: {0}").format(order_data.get("email")))
     else:
         frappe.msgprint(_("Customer already exists for email: {0}").format(order_data.get("email")))
+        
+
+@frappe.whitelist(allow_guest=True)
+def product_creation():
+    order_data = frappe.local.request.get_json()
+    product_id = order_data.get("id")
+    sys_lang = frappe.get_single("System Settings").language or "en"
+    status = False
+    images_src = []
+    settings = frappe.get_doc("Shopify Connector Setting")
+    price = 0
+    for prices in order_data.get("variants"):
+       price =  prices.get("price")
+
+    
+    if order_data.get("status") == "draft":
+        status = True
+
+    if frappe.db.get_value("Item", {"shopify_id": product_id}):
+        return "Order already exists."
+
+    
+    item = frappe.new_doc("Item")
+    item.item_code = f"Shopify-{product_id}"
+    item.item_name= order_data.get("title")
+    item.item_group =  _( "Shopify Products", sys_lang)
+    item.stock_uom= settings.uom
+    item.shopify_id=order_data.get("product_id")
+    item.shopify_selling_rate = price
+    item.disabled = status
+    response = requests.get(f'https://{settings.shop_url}/admin/api/2021-10/products/{product_id}.json', headers={'X-Shopify-Access-Token': settings.access_token})
+    if response.status_code == 200:
+        product_data = response.json()['product']
+        images_src += [image['src'] for image in product_data['images']]
+    else:
+        print(f"Failed to fetch product details: {response.status_code} - {response.text}")
+    img_link  = images_src[0] if len(images_src)>0 else ''
+    
+    file_doc = frappe.new_doc("File")
+    file_doc.file_url = img_link
+    file_doc.is_private = 0
+    file_doc.flags.ignore_permissions = True
+    file_doc.insert(ignore_permissions=True)
+    file_doc.save()
+    item.image = file_doc.file_url if file_doc else '' 
+                
+    item.flags.ignore_permission = True
+    item.insert(ignore_permissions=True)
+    item.save()
+        
+
